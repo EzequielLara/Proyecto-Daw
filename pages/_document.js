b8138e07b4ea10d1c5f5b7ce587f3e0b142c3ffa
EzequielLara <44619010+EzequielLara@users.noreply.github.com>
2023-05-04 15:11:53 +0200

    nuevo intento

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link rel="stylesheet" href="/fonts/FredokaOne-Regular.ttf" />
         <style>
           {`
           @font-face {
